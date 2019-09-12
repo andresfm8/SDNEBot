75c443ffcb7d50edf3075ebd8cc78d945e1472c5
Jacob Brasil <jnxtbrasil@gmail.com>
2019-09-12 19:33:00 -0400

    Fixed

--- bot.js
+++ bot.js
@@ -140,7 +140,7 @@ bot.on('message', msg => {
     } else {
       msg.reply("that is an invalid campus. <:catSad:619611587577249853>");
     }
-  } else if ((!msg.content.startsWith("!year ") && !msg.content.startsWith("!campus ")) && msg.channel.id == "619603429379014667") {
+  } else if (!msg.content.startsWith("!year ") && !msg.content.startsWith("!campus ") && msg.channel.id == "619603429379014667" && msg.author.id != "619590949303091211") {
     msg.delete();
     msg.reply("this channel is for assigning your year and campus only.").then(msg => {
       setTimeout(() => {
