622265039ba9a750086096ea5871f04dc7ca1f84
Jacob Brasil <jnxtbrasil@gmail.com>
2020-04-23 17:21:32 -0400

    Added Bot Token in hidden file

--- bot.ts
+++ bot.ts
@@ -2,13 +2,15 @@
 import * as Discord from 'discord.js'
 
 import * as db from './database'
-import { roles } from './globVars'
+import { botToken } from './env'
+import { roles } from './lib/globVars'
 import { handleReactionAdd } from './lib/handlers/reactions'
 import { handleMessage } from './lib/handlers/messages'
 
 // Instances
 export const bot = new Discord.Client({ partials: Object.values(Discord.Constants.PartialTypes) })
 
+// Bot startup
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}`)
 
@@ -22,15 +24,11 @@ bot.on('ready', () => {
     })
 })
 
-//#region Event Listeners
-
 // Listen for Messages
 bot.on('message', message => { handleMessage(message) })
 
 // Listen for Reactions
 bot.on('messageReactionAdd', (reaction, user) => { handleReactionAdd(reaction, user) })
 
-//#endregion
-
 // Login Bot
-bot.login('NzAyMTkwNTIxNzMyMzAwODIw.Xp82MA.b3lKG2D8nniuMFq91BTJ_kvtFMQ')
+bot.login(botToken)
