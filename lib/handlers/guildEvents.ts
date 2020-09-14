5c447728462e113e5e3665e622af88433ee625e5
Jacob Brasil <jnxtbrasil@gmail.com>
2020-09-14 10:09:14 -0400

    GuildEvents wait for user to be fetched

--- lib/handlers/guildEvents.ts
+++ lib/handlers/guildEvents.ts
@@ -5,26 +5,26 @@ import * as db from '../../database'
 import { bot } from '../../bot'
 import { roles } from '../globVars'
 
-export async function handleNewMember(member: Discord.GuildMember | Discord.PartialGuildMember) {
-    if (member.partial) await member.fetch()
-
-    db.updateUser(member.id, member.user.username)
-    let id = await db.getConfig('assignChannel')
-
-    let members = await member.guild.members.fetch()
-
-    let admins: string = ''
-
-    members.array().forEach(member => {
-        if (member.hasPermission(Permissions.FLAGS.ADMINISTRATOR) && !member.user.bot) {
-            if (admins === '')
-                admins += `<@${member.id}>`
-            else
-                admins += `, <@${member.id}>`
-        }
+export function handleNewMember(member: Discord.GuildMember | Discord.PartialGuildMember) {
+    member.fetch().then(async mem => {
+        db.updateUser(mem.id, mem.user.username)
+        let id = await db.getConfig('assignChannel')
+    
+        let members = await mem.guild.members.fetch()
+    
+        let admins: string = ''
+    
+        members.array().forEach(m => {
+            if (m.hasPermission(Permissions.FLAGS.ADMINISTRATOR) && !m.user.bot) {
+                if (admins === '')
+                    admins += `<@${m.id}>`
+                else
+                    admins += `, <@${m.id}>`
+            }
+        })
+    
+        mem.send(`**Welcome to the SDNE Discord!**\nMake sure to assign your year and campus using the corresponding\nreactions in <#${id}> in order to access all the channels!\nDM ${admins} if you need help.`)
+    
+        mem.roles.add(roles['👻']).catch(err => console.error(err))
     })
-
-    member.send(`**Welcome to the SDNE Discord!**\nMake sure to assign your year and campus using the corresponding\nreactions in <#${id}> in order to access all the channels!\nDM ${admins} if you need help.`)
-
-    member.roles.add(roles['👻']).catch(err => console.error(err))
 }
