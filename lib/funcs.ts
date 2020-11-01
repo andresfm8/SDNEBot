1b28fa4b5c148cf2cf84d761027463c6162653ff
N3rdP1um23 <james@matrek.com>
2020-11-01 12:08:42 -0500

    Updated the functions file and also sped us some logical statements

--- lib/funcs.ts
+++ lib/funcs.ts
@@ -1,49 +1,84 @@
+/**
+ *
+ * TimmyRB
+ * November 1, 2020
+ * The following file is used for function initialization for the application
+ *
+ */
+
+// Import the required items
 import * as Discord from 'discord.js'
 import { Permissions } from 'discord.js'
 import * as db from '../database'
 
+/**
+ *
+ * The following function is used to render the help menu
+ *
+ * @param member: is the user that has run the command
+ * @param page: is the page that is requested from the help menu
+ * @param channel: is the chanel for which the command was executed in
+ * @param msg: is the message that was executed by the user
+ *
+ */
 export function help(member: Discord.GuildMember, page: number, channel?: Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel, msg?: Discord.Message): number {
+	// Define the required variables
+	var tips_per_page = 3;
 
+	// Initialize the tips array that will hold all of the help tips
 	let tips = [
 		{
-			'name': 'View Help',
-			'value': '```!help```'
+			name: 'View Help',
+			value: '```!help```'
 		},
 		{
-			'name': 'Pin Message',
-			'value': '```React with 📌 on any message to save it```'
+			name: 'Pin Message',
+			value: '```React with 📌 on any message to save it```'
 		}
-	]
+	];
 
-	tips.push({ 'name': 'Create Channel', 'value': '```!make channelName```'})
+	// Append additional tips
+	tips.push({ 'name': 'Create Channel', 'value': '```!make channelName```'});
+	tips.push({ 'name': 'User(s\') Info', 'value': '```!info @user[]```' });
+	tips.push({ 'name': 'View RateMyProfessor info', 'value': '```!rmp profName```'});
+	tips.push({ 'name': 'Get days until an event (mainly holiday)', 'value': '```!daysUntil event name```'});
 
-	tips.push({ 'name': 'User(s\') Info', 'value': '```!info @user[]```' })
-
-	tips.push({ 'name': 'View RateMyProfessor info', 'value': '```!rmp profName```'})
-
-	if (member.hasPermission(Permissions.FLAGS.MANAGE_ROLES))
-		tips.push({ 'name': 'Assign Roles Embed', 'value': '```!assignInfo```' })
-
-	if (member.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)) {
-		tips.push({ 'name': 'Cleanup Messages', 'value': '```!cleanup #num_messages```' })
+	// Check to see if the member has the permission to manage roles
+	if(member.hasPermission(Permissions.FLAGS.MANAGE_ROLES)) {
+		// Append the assign roles tip
+		tips.push({ 'name': 'Assign Roles Embed', 'value': '```!assignInfo```' });
 	}
 
-	if (member.hasPermission(Permissions.FLAGS.KICK_MEMBERS)) {
-		tips.push({ 'name': 'Mute User(s)', 'value': '```!mute @non_admin_user[]```' })
-		tips.push({ 'name': 'Unmute User(s)', 'value': '```!unmute @non_admin_user[]```' })
-		tips.push({ 'name': 'Warn User(s)', 'value': '```!warn #rule_num @non_admin_user[]```' })
+	// Check to see if the member has the permission to manage messages
+	if(member.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)) {
+		// Append the cleanup messages tip
+		tips.push({ 'name': 'Cleanup Messages', 'value': '```!cleanup #num_messages```' });
 	}
 
-	tips.push({ 'name': 'Get days until an event (mainly holiday)', 'value': '```!daysUntil event name```'})
+	// Check to see if the member has the permission to kick members
+	if(member.hasPermission(Permissions.FLAGS.KICK_MEMBERS)) {
+		// Append the respective mute, warn, and kick tips
+		tips.push({ 'name': 'Mute User(s)', 'value': '```!mute @non_admin_user[]```' });
+		tips.push({ 'name': 'Unmute User(s)', 'value': '```!unmute @non_admin_user[]```' });
+		tips.push({ 'name': 'Warn User(s)', 'value': '```!warn #rule_num @non_admin_user[]```' });
+	}
 
-	let maxPages = Math.ceil(tips.length / 3)
+	// Define the max amount of pages  based on the amount of tips
+	let maxPages = Math.ceil(tips.length / tips_per_page);
 
-	if (page > maxPages)
-		page = maxPages
+	// Check to see if the current page is great than the max amount of pages
+	if(page > maxPages) {
+		// Reset the page back to the max allowed page
+		page = maxPages;
+	}
 
-	if (page < 1)
-		page = 1
+	// Check to see if the current page is less than 1
+	if(page < 1) {
+		// Reset the page to the first page
+		page = 1;
+	}
 
+	// Start to formulate the embed object
 	let embed = {
 		'embed': {
 			'title': 'Help Menu',
@@ -52,27 +87,65 @@ export function help(member: Discord.GuildMember, page: number, channel?: Discor
 			'timestamp': new Date(),
 			'fields': []
 		}
+	};
+
+	// Determine the current page of tips to display
+	let toDisplay = [tips.slice((page - 1) * 3), 3];
+
+	// Push the items to display to the embeded fields array
+	toDisplay.forEach(item => {
+		// Check to make aure that the item isn't undefined
+		if(item !== undefined) {
+			// Push the item to the embed fields array
+			embed['embed']['fields'].push(item);
+		}
+	});
+
+	// Check to make sure that the channel isn't undefined
+	if(channel !== undefined) {
+		// Send the new embed object to the specified channel
+		channel.send(embed).then(m => {
+			// React with the page left option
+			m.react('⬅️').then(() => {
+				// React with the page right option
+				m.react('➡️').then(() => {
+					// React with the help remove option
+					m.react('🗑️');
+				});
+			});
+
+			// Update the config value that stores the latest help message id
+			db.updateConfig('help', m.id);
+		});
+	}else if(msg !== undefined) {
+		// Update the embeded message
+		msg.edit(embed);
 	}
 
-	let toDisplay = [tips[((page - 1) * 3)], tips[((page - 1) * 3) + 1], tips[((page - 1) * 3) + 2]]
-
-	toDisplay.forEach(t => {
-		if (t !== undefined)
-			embed['embed']['fields'].push(t)
-	})
-
-	if (channel !== undefined)
-		channel.send(embed).then(m => { m.react('⬅️').then(() => { m.react('➡️').then(() => { m.react('🗑️') }) }); db.updateConfig('help', m.id) })
-	else if (msg !== undefined)
-		msg.edit(embed)
-
-	return page
+	// Return the page that the user is currently on
+	return page;
 }
 
+/**
+ *
+ * The following function is used to determine if the message has an attachment or not
+ *
+ * @param msg: is the message that is to be checked
+ *
+ */
 export function hasAttachment(msg: Discord.Message | Discord.PartialMessage): boolean {
-	return (msg.attachments.size > 0)
+	// Return if the message has an attachment
+	return (msg.attachments.size > 0);
 }
 
+/**
+ *
+ * The following function is used to determine if the message has an URL or not
+ *
+ * @param msg: is the message that is to be checked
+ *
+ */
 export function hasURL(msg: Discord.Message | Discord.PartialMessage): boolean {
-	return (msg.content.indexOf('https://') !== -1 || msg.content.indexOf('http://') !== -1)
+	// Return if the message has a URL
+	return (msg.content.indexOf('https://') !== -1 || msg.content.indexOf('http://') !== -1);
 }
