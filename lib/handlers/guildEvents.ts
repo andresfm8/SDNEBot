267571174dba66c873c9a853a0fb16a2b1b233d3
N3rdP1um23 <james@matrek.com>
2020-11-01 12:19:35 -0500

    Updated the guildEvents file

--- lib/handlers/guildEvents.ts
+++ lib/handlers/guildEvents.ts
@@ -1,30 +1,37 @@
-import * as Discord from 'discord.js'
-import { Permissions } from 'discord.js'
+/**
+ *
+ * TimmyRB
+ * November 1, 2020
+ * The following file is used for handling functions related to guild events
+ *
+ */
 
-import * as db from '../../database'
-import { bot } from '../../bot'
-import { roles } from '../globVars'
+// Import the required items
+import * as Discord from 'discord.js';
+import { Permissions } from 'discord.js';
+import * as db from '../../database';
+import { bot } from '../../bot';
+import { roles } from '../globVars';
 
+/**
+ *
+ * The following function is used to handle when new members join the server
+ *
+ * @param member: is the new member to handle
+ */
 export function handleNewMember(member: Discord.GuildMember | Discord.PartialGuildMember) {
+	// Fetch the member object from Discord
 	member.fetch().then(async mem => {
-		db.updateUser(mem.id, mem.user.username)
-		let id = await db.getConfig('assignChannel')
+		// Update the user in the database
+		db.updateUser(mem.id, mem.user.username);
 
-		let members = await mem.guild.members.fetch()
+		// Create the required variabels
+		let id = await db.getConfig('assignChannel'); // Get the assignChannel channel id
 
-		let admins: string = ''
+		// Send a DM to the user welcoming them to the server and also mentioning to assign roles and contact an admin for more support
+		mem.send(`**Welcome to the SDNE Discord!**\nMake sure to assign your year and campus using the corresponding\nreactions in <#${id}> in order to access all the channels!\nDM an admin if you need help.`)
 
-		members.array().forEach(m => {
-			if (m.hasPermission(Permissions.FLAGS.ADMINISTRATOR) && !m.user.bot) {
-				if (admins === '')
-					admins += `<@${m.id}>`
-				else
-					admins += `, <@${m.id}>`
-			}
-		})
-
-		mem.send(`**Welcome to the SDNE Discord!**\nMake sure to assign your year and campus using the corresponding\nreactions in <#${id}> in order to access all the channels!\nDM ${admins} if you need help.`)
-
-		mem.roles.add(roles['👻']).catch(err => console.error(err))
-	})
+		// Add the unassigned role to the user since their new
+		mem.roles.add(roles['👻']).catch(error => console.error(error));
+	});
 }
